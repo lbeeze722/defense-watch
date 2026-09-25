4ede22e47553490ae052bde598e7fb1332bcde23
lbeeze722 <lbeeze722@users.noreply.github.com>
2026-09-25 09:14:59 -0700

    data pulse

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.DASHBOARD_DATA = {
- "generatedAt": "2026-09-25T16:09:45.363271+00:00",
+ "generatedAt": "2026-09-25T16:14:59.281119+00:00",
  "threshold": 3.0,
  "verticalOrder": [
   "Prime Contractors",
